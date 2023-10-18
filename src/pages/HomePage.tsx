ead1d79d641763410210229f4f9e0ae3f29251d2
rohitAmdahl <rohit_designer@outlook.com>
2023-10-18 02:12:47 +0200

    smal change react.FC

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import FetchData from "../api/FetchData";
 
-// import FetchData from "../api/FetchData";
-const HomePage = () => {
+const HomePage: React.FC = () => {
   return (
     <div className="grid grid-cols-1 gap-2 lg:grid-cols-3 md:grid-cols-2 container mx-auto max-w-5xl lg:gap-5 m-8 p-5">
       <FetchData />
